87225066ba3c4108d483534302a04795ac04d798
Luping <lupingxie@LupingdeMacBook-Pro.local>
2025-06-19 22:08:19 +0800

    test

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ export async function run(): Promise<void> {
   try {
     const diffFiles: string = core.getInput('diff_files')
     const changedFiles: string[] = JSON.parse(diffFiles)
-    core.info(`Changed files: ${changedFiles.join(', ')}`)
+    core.info(`Changed files ${changedFiles}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
