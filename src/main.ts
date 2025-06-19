1c41c501e5489dcf98ed94c1bdfd16594033e2cf
Luping <lupingxie@LupingdeMacBook-Pro.local>
2025-06-19 21:40:54 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+// import { wait } from './wait.js'
 
 /**
  * The main function for the action.
@@ -8,18 +8,9 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const diffFiles: string = core.getInput('diff_files')
+    const changedFiles: string[] = JSON.parse(diffFiles)
+    core.info(`Changed files: ${changedFiles.join(', ')}`)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
